54ac5df297b749dc4e2b2ba62e5ba893365199e3
Luke Xue <luqi9900@gmail.com>
2022-08-30 01:27:55 -0600

    basic design

--- sortingalgos/views.py
+++ sortingalgos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
+def arrayValues():
+    ary = []
+    for i in range(0, 150):
+        n = random.randint(100, 600)
+        ary.append(n)
+    return ary
+
+
+def index(request):
+    global arr
+    arr = arrayValues()
+    return render(request, "index.html", {'arr': arr})
